b9daa5e38794f7375e27185a527965dcd10e01a0
Kyle Nixon <nixon.a.kyle@gmail.com>
2025-07-02 12:38:22 -0400

    set up routing with home and about pages

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>About Page</h1>
+        <p>This is an about page for your guild activity tracker app.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
