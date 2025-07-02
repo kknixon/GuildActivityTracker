01a1764e9dc66fbceb6dc1a44cb427fe7ad5db51
Kyle Nixon <nixon.a.kyle@gmail.com>
2025-07-02 13:05:06 -0400

    saved changes for routing

--- src/app.vue
+++ src/app.vue
@@ -1,27 +1,16 @@
 <template>
     <div id="app">
-<<<<<<< HEAD
-        <h1>Welcome to Your Vue.js App</h1>
-=======
         <nav>
             <router-link to="/">Home</router-link>
             <router-link to="/about">About</router-link>
         </nav>
         <router-view />
->>>>>>> feature/add-routing
     </div>
 </template>
 
 <script>
 export default {
     name: 'App',
-<<<<<<< HEAD
-}
-</script>
-
-<style>
-/* Add your styles here */
-=======
 };
 </script>
 
@@ -35,5 +24,4 @@ nav a {
     margin: 0 10px;
     text-decoration: none;
 }
->>>>>>> feature/add-routing
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import { createApp } from "vue";
-<<<<<<< HEAD
-import App from './app.vue'
-
-createApp(App).mount('#app')
-=======
 import App from './app.vue';
 import router from './router';
 
 createApp(App)
     .use(router)
     .mount('#app')
->>>>>>> feature/add-routing
